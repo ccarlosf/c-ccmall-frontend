d1857c6bb390168cfddfc2f25298accea8dd0a3f
ccarlos <2238513100@qq.com>
2019-11-09 21:46:14 +0800

    spu-scroll主题商品数据显示

--- pages/home/home.js
+++ pages/home/home.js
@@ -29,11 +29,11 @@ Page({
 
         const themeA = await theme.getHomeLocationA()
         const themeE=await theme.getHomeLocationE()
-        let themeESpu=[]
+        let themeESpu=[];
         if(themeE.online){
-            const data=await Theme.getHomeLocationESpu()
+            const data=await Theme.getHomeLocationESpu();
             if(data){
-                themeESpu=data.spu_list.slice(0,8)
+                themeESpu = data.spu_list.slice(0,8)
             }
         }
         const bannerB = await Banner.getHomeLocationB()
