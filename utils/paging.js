8bea2cf487735fb3808a063cd45392e14113dc01
ccarlos <2238513100@qq.com>
2019-11-17 21:43:23 +0800

    (3 2-5 分页对象Paging封装二)

--- utils/paging.js
+++ utils/paging.js
@@ -3,23 +3,28 @@
  * @author: ccarlos
  * @date 2019/11/17 21:02
  */
+import {Http} from "./Http";
+
 class Paging {
 
     //状态 实例化 new Paging
 
     start
     count
-    locker
+    req
+    locker = false
+    url
 
     /**
      * @description: 构造器
      * @author: ccarlos
      * @date 2019/11/17 21:09
      */
-    constructor(url, count = 10, start = 0) {
-        this.url = url
+    constructor(req, count = 10, start = 0) {
+        this.req = req
         this.count = count
         this.start = start
+        this.url = req.url
     }
 
 
@@ -32,6 +37,37 @@ class Paging {
         //getLocker
         //request
         //releaseLocker
+        if (!this._getLocker()) {
+            return
+        }
+        this._actualGetData()
+        this._releaseLocker()
+    }
+
+    /**
+     * @description: 真实获取数据
+     * @author: ccarlos
+     * @date 2019/11/17 21:26
+     */
+    _actualGetData() {
+        Http.request()
+    }
+
+    /**
+     * @description: 获取当前请求对象
+     * @author: ccarlos
+     * @date 2019/11/17 21:34
+     */
+    _getCurrentReq() {
+        let url = this.url
+        const params = `start=${this.start}&count=${this.count}`
+        if (url.indexOf('?') !== -1) {
+            url += '&' + params
+        } else {
+            url += '?' + params
+        }
+        this.req.url =url
+        return this.req
     }
 
     /**
@@ -39,7 +75,7 @@ class Paging {
      * @author: ccarlos
      * @date 2019/11/17 21:09
      */
-    getLocker() {
+    _getLocker() {
         if (this.locker) {
             return false
         }
@@ -47,7 +83,7 @@ class Paging {
         return true
     }
 
-    releaseLocker() {
+    _releaseLocker() {
         this.locker = false
     }
 
