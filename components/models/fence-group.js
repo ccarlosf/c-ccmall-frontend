2fc2807ee31218dc494d67aeb920d87b6a31a2bb
ccarlos <2238513100@qq.com>
2019-11-30 23:06:50 +0800

    (4 3-5)矩阵的遍历

--- components/models/fence-group.js
+++ components/models/fence-group.js
@@ -3,6 +3,8 @@
  * @author: ccarlos
  * @date 2019/11/28 23:39
 */
+import {Matrix} from "./matrix";
+
 class FenceGroup {
     spu
     skuList = []
@@ -17,7 +19,34 @@ class FenceGroup {
         this.skuList = spu.sku_list
     }
 
-    initFences
+    /**
+     * @description: fences初始化
+     * @author: ccarlos
+     * @date 2019/11/30 22:38
+    */
+    initFences(){
+        const matrix=this._createMatrix(this.skuList)
+        matrix.forEach((element,i,j)=>{
+
+        })
+        //for
+    }
+
+    /**
+     * @description: 创建矩阵
+     * @author: ccarlos
+     * @date 2019/11/30 22:39
+    */
+    _createMatrix(skuList){
+        const  m = []
+        skuList.forEach(sku=>{
+            m.push(sku.specs)
+        })
+        return new Matrix(m)
+    }
+
+    // 1.数学函数库 JS 体积太大 mathjs
+    // 2.不用数学函数库，借助矩阵思维
 }
 
 export {
---
+++ components/models/matrix.js
@@ -0,0 +1,54 @@
+/**
+ * @description: 矩阵matrix类
+ * @author: ccarlos
+ * @date 2019/11/30 22:42
+ */
+class Matrix {
+    m
+
+    /**
+     * @description: 构造函数
+     * @author: ccarlos
+     * @date 2019/11/30 22:51
+     */
+    constructor(matrix) {
+        this.m = matrix
+    }
+
+    /**
+     * @description: 获取矩阵的行数
+     * @author: ccarlos
+     * @date 2019/11/30 22:51
+     */
+    get rowsNum() {
+        return this.m.length
+    }
+
+    /**
+     * @description: 获取矩阵的列数
+     * @author: ccarlos
+     * @date 2019/11/30 22:52
+     */
+    get colsNum() {
+        return this.m[0].length
+    }
+
+    /**
+     * @description: 循环处理矩阵
+     * @author: ccarlos
+     * @date 2019/11/30 22:53
+     */
+    forEach(cb) {
+        //return
+        for (let j = 0; j < this.colsNum; j++) {
+            for (let i = 0; i < this.rowsNum; i++) {
+                const element = this.m[i][j]
+                cb(element, i, j)
+            }
+        }
+    }
+}
+
+export {
+    Matrix
+}
