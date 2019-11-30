1b61bbc9485feaf13a4b172e1965990ca3385ba3
ccarlos <2238513100@qq.com>
2019-11-30 21:47:00 +0800

    (4 3-3) 用矩阵的思想提取规格值

--- components/models/fence-group.js
+++ components/models/fence-group.js
@@ -3,3 +3,23 @@
  * @author: ccarlos
  * @date 2019/11/28 23:39
 */
+class FenceGroup {
+    spu
+    skuList = []
+
+    /**
+     * @description: 构造函数
+     * @author: ccarlos
+     * @date 2019/11/30 21:30
+     */
+    constructor(spu) {
+        this.spu = spu
+        this.skuList = spu.sku_list
+    }
+
+    initFences
+}
+
+export {
+    FenceGroup
+}
--- components/realm/index.js
+++ components/realm/index.js
@@ -15,6 +15,33 @@ Component({
   },
 
   /**
+   * @description: 生命周期函数
+   * @author: ccarlos
+   * @date 2019/11/30 21:28
+  */
+  lifetimes:{
+    attached(){
+
+    },
+    //ready
+
+    //create
+  },
+
+  /**
+   * @description: 监听器
+   * @author: ccarlos
+   * @date 2019/11/30 21:25
+  */
+  observers:{
+    'spu':function (spu) {
+      if(!spu){
+        return
+      }
+    }
+  },
+
+  /**
    * 组件的方法列表
    */
   methods: {
