cfb031bdfe363af1715a7ee0f433d94612cc0a13
ccarlos <2238513100@qq.com>
2019-12-08 15:40:36 +0800

    (5 2-6) 选中、禁用与可选状态的切换

---
+++ components/core/enum.js
@@ -0,0 +1,14 @@
+/**
+ * @description: 枚举常量类
+ * @author: ccarlos
+ * @date 2019/12/8 15:24
+ */
+const CellStatus = {
+    FORBIDDEN: 'forbidden',
+    SELECTED: 'selected',
+    WAITING: 'waiting'
+}
+
+export {
+    CellStatus
+}
--- components/models/cell.js
+++ components/models/cell.js
@@ -3,9 +3,12 @@
  * @author: ccarlos
  * @date 2019/12/1 13:09
 */
+import {CellStatus} from "../core/enum";
+
 class Cell {
     title
     id
+    status = CellStatus.WAITING
 
     /**
      * @description: 构造函数
