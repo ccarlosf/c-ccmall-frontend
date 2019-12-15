9756f5e0bcf14f398032f3f2a633ec0b51c1ef3d
ccarlos <2238513100@qq.com>
2019-12-15 20:16:32 +0800

    (6 2-2) 详解l-counter的数量控制属性

--- components/counter/index.js
+++ components/counter/index.js
@@ -1,10 +1,24 @@
 // components/counter/index.js
+
+import {Cart} from "../../models/cart";
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    count:{
+      type:Number,
+      value:Cart.SKU_MIN_COUNT
+    },
+    min:{
+      type:Number,
+      value:Cart.SKU_MIN_COUNT
+    },
+    max:{
+      type:Number,
+      value: Cart.SKU_MAX_COUNT
+    }
   },
 
   /**
@@ -19,5 +33,27 @@ Component({
    */
   methods: {
 
+    /**
+     * @description: 超出购买数量事件触发函数
+     * @author: ccarlos
+     * @date 2019/12/15 20:09
+    */
+    onOverStep(event){
+      const minOrMaxOut =event.detail.type
+      if(minOrMaxOut === 'overflow_max'){
+        wx.showToast({
+          icon:"none",
+          duration:3000,
+          title:'超出最大购买数量'
+        })
+      }
+      if(minOrMaxOut === 'overflow_min'){
+        wx.showToast({
+          icon: "none",
+          duration: 3000,
+          title: `最少需要购买${Cart.SKU_MIN_COUNT}件噢`
+        })
+      }
+    }
   }
 })
---
+++ models/cart.js
@@ -0,0 +1,13 @@
+/**
+ * @description: 购物车类
+ * @author: ccarlos
+ * @date 2019/12/15 19:58
+ */
+class Cart {
+    static SKU_MIN_COUNT = 1
+    static SKU_MAX_COUNT = 99
+}
+
+export {
+    Cart
+}
