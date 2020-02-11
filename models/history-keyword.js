664c34b1ca8e09291a07f5ba07d7c4a3497625c0
ccarlos <2238513100@qq.com>
2020-02-11 21:06:41 +0800

    (7 1-3) 数据结构队列在HistoryKeyword中应用

--- models/history-keyword.js
+++ models/history-keyword.js
@@ -4,5 +4,33 @@
  * @date: 2020/2/10 23:13
  */
 class HistoryKeyword{
+    static MAX_ITEM_COUNT = 20
 
+    keywords = []
+
+// 不存在实例属性
+// 单例模式
+
+    // 缓存中写入数据
+    // 去重
+    save(keyword) {
+        const items = this.keywords.filter(k => {
+            return k === keyword
+        })
+        if (items.length !== 0) {
+            return
+        }
+        if (this.keywords.length >= HistoryKeyword.MAX_ITEM_COUNT) {
+            this.keywords.pop()
+        }
+        this.keywords.unshift(keyword)
+    }
+
+    get() {
+        return this.keywords
+    }
+
+    clear() {
+
+    }
 }
